845d7c6dfa39e2c3af8a72226b2e2bb36d4d31b0
Lou Batier <batierlou@gmail.com>
2021-04-25 09:50:02 +0200

    tweaking

--- components/PlanetModal.vue
+++ components/PlanetModal.vue
@@ -2,28 +2,28 @@
     <section v-if="isDisplayed">
         <div class="ui-gradient ui-gradient--top"></div>
         <div class="wrapper">
-            <h2>{{ content.title }}</h2>
+            <h2>{{ planetContent.title }}</h2>
             <div class="container">
                 <div class="text-content">
-                    <p>Née dans les années 1960, la mini-jupe est devenue en France un symbole stylistique célébrant une femme affranchie des codes vestimentaires conservateurs.</p>
-                    <p>Si aujourd'hui on la croise sans qu'elle ne fasse sourciller, il n'en n'a pas toujours été ainsi. Lorsqu'elle débarque sur le sol français en 1964, la mini-jupe est un OMNI (objet mode non-identifié) qui, s'il séduit la jeunesse s'attire rapidement les foudres des plus conservateurs.</p>
-                    <p>Pour bien comprendre le bouleversement occasionné par ce petit bout de tissu, il faut rappeler qu'à cette époque, les femmes portaient des jupes longues et que le comble de l'indécence était de laisser apparaître un genou. Dans cette ère ultra-conservatrice, une femme a pourtant osé braver les interdits.</p>
+                    <p>{{ planetContent.paragraphs[0] }}</p>
+                    <p>{{ planetContent.paragraphs[1] }}</p>
+                    <p>{{ planetContent.paragraphs[2] }}</p>
                 </div>
                 <div class="img-content">
-                    <div class="img img--large"></div>
-                    <div class="img img--small"></div>
-                    <div class="img img--small"></div>
+                    <div class="img img--large">{{ planetContent.images[0] }}</div>
+                    <div class="img img--small">{{ planetContent.images[1] }}</div>
+                    <div class="img img--small">{{ planetContent.images[2] }}</div>
                 </div>
             </div>
             <div class="container">
-                <div class="video"></div>
+                <div class="video">{{ planetContent.video }}</div>
             </div>
             <div class="container">
                 <div class="text-content">
-                    <p>Elle s'appelle Mary Quant. Elle est britannique et styliste de profession. Pour séduire les clientes de sa boutique, la jeune femme décide de raccourcir les jupes qu'elle vend et d'appeler sa création "mini-jupe" en référence, dit-on, à la voiture Mini dont elle raffole.</p>
+                    <p>{{ planetContent.paragraphs[3] }}</p>
                 </div>
                 <div class="text-content">
-                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor.</p>
+                    <p>{{ planetContent.paragraphs[4] }}</p>
                 </div>
             </div>
             <button @click="isDisplayed = false">Terminer la lecture</button>
@@ -39,8 +39,21 @@ export default {
     data() {
         return {
             isDisplayed: true,
-            content: {
-                title : ""
+            planetContent: {
+                title : "Jupe",
+                paragraphs: [
+                    "Née dans les années 1960, la mini-jupe est devenue en France un symbole stylistique célébrant une femme affranchie des codes vestimentaires conservateurs.",
+                    "Si aujourd'hui on la croise sans qu'elle ne fasse sourciller, il n'en n'a pas toujours été ainsi. Lorsqu'elle débarque sur le sol français en 1964, la mini-jupe est un OMNI (objet mode non-identifié) qui, s'il séduit la jeunesse s'attire rapidement les foudres des plus conservateurs.",
+                    "Pour bien comprendre le bouleversement occasionné par ce petit bout de tissu, il faut rappeler qu'à cette époque, les femmes portaient des jupes longues et que le comble de l'indécence était de laisser apparaître un genou. Dans cette ère ultra-conservatrice, une femme a pourtant osé braver les interdits.",
+                    "Elle s'appelle Mary Quant. Elle est britannique et styliste de profession. Pour séduire les clientes de sa boutique, la jeune femme décide de raccourcir les jupes qu'elle vend et d'appeler sa création \"mini-jupe\" en référence, dit-on, à la voiture Mini dont elle raffole.",
+                    "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor.",
+                ],
+                images: [
+                    "URL de l'image",
+                    "URL de l'image",
+                    "URL de l'image",
+                ],
+                video: "URL de la vidéo"
             }
         }
     },
@@ -130,7 +143,7 @@ export default {
     }
 
     .img {
-        background-color: red;
+        background-color: #7956b2;
         width: 50px;
         height: 50px;
         border-radius: 1rem;
@@ -148,7 +161,7 @@ export default {
     }
 
     .video {
-        background-color: blue;
+        background-color: #56b279;
         width: 100%;
         height: 300px;
         border-radius: 1rem;
